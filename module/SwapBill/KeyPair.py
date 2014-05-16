844bed3595d8fe9093ceded4c90920c2836ca1c8
Thomas Young <thomas@upcoder.com>
2014-05-16 18:40:27 +0200

    worked through key pair generation and use with raw transactions api and sent transaction successfully, record currently there in comments

--- module/SwapBill/KeyPair.py
+++ module/SwapBill/KeyPair.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import ecdsa, hashlib
+import ecdsa, hashlib, random, binascii
 from SwapBill import Base58Check
 
 class BadVersionNumber(Exception):
@@ -9,10 +9,21 @@ def generatePrivateKey():
 	# Warning: this random function is not cryptographically strong and is just for example
 	# (but using for now anyway, just to see things working!)
 	privateKeyHex = ''.join(['%x' % random.randrange(16) for x in range(0, 64)])
-	return privateKeyHex.decode('hex')
-def privateKeyFromWIF(wif):
+	return binascii.unhexlify(privateKeyHex.encode('ascii'))
+def privateKeyFromWIF(addressVersion, wif):
 	data = Base58Check.Decode(wif)
-	if data[:1] != b'\x80':
+	if data[:1] != addressVersion:
+		raise BadVersionNumber()
+	return data[1:]
+def privateKeyToWIF(data, addressVersion):
+	assert type(addressVersion) is type(b'.')
+	assert type(data) is type(b'.')
+	assert len(addressVersion) == 1
+	assert len(data) == 32
+	return Base58Check.Encode(addressVersion + data)
+
+	data = Base58Check.Decode(wif)
+	if data[:1] != addressVersion:
 		raise BadVersionNumber()
 	return data[1:]
 
@@ -23,5 +34,5 @@ def privateKeyToPublicKey(privateKey):
 
 def publicKeyToPubKeyHash(publicKey):
 	ripemd160 = hashlib.new('ripemd160')
-	ripemd160.update(hashlib.sha256(publicKey).digest())
+	ripemd160.update(hashlib.sha256(b'\x04' + publicKey).digest())
 	return ripemd160.digest()
