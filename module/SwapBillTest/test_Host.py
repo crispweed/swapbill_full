2055feca022da75f69fca070e3bcc12db43eec67
Thomas Young <thomas@upcoder.com>
2014-06-13 13:09:25 +0200

    wip adding unit test for host

--- module/SwapBillTest/test_Host.py
+++ module/SwapBillTest/test_Host.py
@@ -28,6 +28,5 @@ class Test(unittest.TestCase):
 		queryResult = [{u'hex': u'01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4804ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536ffffffff0100f2052a010000004341040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac00000000', u'txid': u'97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9'}]
 		rpcHost._d[expectedQuery] = queryResult
 		host = Host.Host(rpcHost=rpcHost, addressVersion=addressVersion, privateKeyAddressVersion=privateKeyAddressVersion, submittedTransactionsLogFileName=submittedTransactionsLogFileName)
-		rpcHost._connect('9Eh7BzxEJGseHnqB7L8dDH3EQG3iNgnDQBTjdhEhPeDg')
-		unspent = host.getUnspent()
-		self.assertEqual(unspent, '')
+		#unspent = host.getUnspent()
+		#self.assertEqual(unspent, '')
