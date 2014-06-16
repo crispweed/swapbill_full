88c6971367c4ca6bea41a043d554db6a5bccb77f
Thomas Young <thomas@upcoder.com>
2014-06-16 11:17:02 +0200

    added conversions between satoshi amounts and decimal strings without using floating point or division

--- module/SwapBill/Amounts.py
+++ module/SwapBill/Amounts.py
@@ -1,5 +1,33 @@
+from SwapBill.ExceptionReportedToUser import ExceptionReportedToUser
+
 def e(power):
 	return 10**power
 def ToSatoshis(value):
 	assert type(value) is float
 	return int(value * e(8))
+
+def ToString(satoshis):
+	s = str(satoshis)
+	l = len(s)
+	if l < 9:
+		s = '0'*(9-l) + s
+	result = s[:-8]
+	after = s[-8:]
+	while after[-1:] == '0':
+		after = after[:-1]
+	if after:
+		result = result + '.' + after
+	return result
+
+def FromString(s):
+	pos = s.find('.')
+	if pos == -1:
+		satoshisString = s + '0' * 8
+	else:
+		digitsAfter = len(s) - 1 - pos
+		if digitsAfter > 8:
+			raise ExceptionReportedToUser('Too much precision in amount string (a maximum of 8 digits are allowed after the decimal point).')
+		digitsToAdd = 8 - digitsAfter
+		satoshisString = s[:pos] + s[pos+1:] + '0' * digitsToAdd
+	return int(satoshisString)
+
---
+++ module/SwapBillTest/test_Amounts.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import unittest
+from SwapBill import Amounts
+
+roundTripPairs=(
+    ('0', 0),
+    ('1', 100000000),
+    ('1.2', 120000000),
+    ('0.00000004', 4),
+    ('0.0000004', 40),
+    ('1.00000005', 100000005),
+    ('12.3456789', 1234567890),
+)
+
+nonStandardButParsed=(
+    ('.0', 0),
+    ('0.0', 0),
+    ('0.00000000', 0),
+    ('01', 100000000),
+    ('1.20', 120000000),
+    ('01.20', 120000000),
+    ('00.00000004', 4),
+    ('0.00000040', 40),
+    ('0001.00000005', 100000005),
+    ('12.34567890', 1234567890),
+)
+
+class Test(unittest.TestCase):
+
+	def test(self):
+		for s, amount in roundTripPairs:
+			self.assertEqual(Amounts.FromString(s), amount)
+			self.assertEqual(Amounts.ToString(amount), s)
+		for s, amount in nonStandardButParsed:
+			self.assertEqual(Amounts.FromString(s), amount)
