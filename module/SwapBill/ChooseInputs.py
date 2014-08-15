1a57c3779b76e22fb3bb6a02885624d76c5bdaf7
Thomas Young <crispweed@upcoder.com>
2014-08-15 09:57:08 +0200

    simplified ChooseInputs after removal of maxInputs parameter

--- module/SwapBill/ChooseInputs.py
+++ module/SwapBill/ChooseInputs.py
@@ -8,23 +8,11 @@ def ChooseInputs(unspentAmounts, amountRequired):
 		sortedUnspent.append((amount, i))
 	sortedUnspent.sort()
 
-	maxInputs = len(unspentAmounts)
-
 	spent = 0
 	result = []
 
-
-	while spent < amountRequired and len(result) < maxInputs:
+	while spent < amountRequired and len(result) < len(unspentAmounts):
 		toAddI = sortedUnspent[len(result)][1]
-		result.append(toAddI)
-		spent += unspentAmounts[result[-1]]
-
-	startI = 0
-	while spent < amountRequired and startI + len(result) < len(unspentAmounts):
-		spent -= unspentAmounts[result[0]]
-		result = result[1:]
-		startI += 1
-		toAddI = sortedUnspent[startI + len(result)][1]
 		result.append(toAddI)
 		spent += unspentAmounts[result[-1]]
 
