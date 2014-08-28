ea7303eb825a38e97856823e694ffbcdaee5ae23
Thomas Young <crispweed@upcoder.com>
2014-08-28 16:34:12 +0200

    split key pair hooks out into separate module (from keypair tests)

---
+++ module/SwapBillTest/hook_KeyPair.py
@@ -0,0 +1,22 @@
+from SwapBill import KeyPair
+
+nextI = 0
+def GeneratePrivateKey_ForTests():
+	global nextI
+	result = str(nextI) + '-' * 31
+	nextI += 1
+	return result[:32].encode('ascii')
+def PrivateKeyToPublicKey_ForTests(privateKey):
+	s = privateKey.decode('ascii').strip('-')
+	i = int(s)
+	result = str(i) + '+' * 63
+	return result[:64].encode('ascii')
+def PublicKeyToPubKeyHash_ForTests(publicKey):
+	s = publicKey.decode('ascii').strip('+')
+	i = int(s)
+	result = str(i) + '*' * 19
+	return result[:20].encode('ascii')
+
+KeyPair.GeneratePrivateKey = GeneratePrivateKey_ForTests
+KeyPair.PrivateKeyToPublicKey = PrivateKeyToPublicKey_ForTests
+KeyPair.PublicKeyToPubKeyHash = PublicKeyToPubKeyHash_ForTests
--- module/SwapBillTest/test_KeyPair.py
+++ module/SwapBillTest/test_KeyPair.py
@@ -1,27 +1,7 @@
 from __future__ import print_function
-import unittest, os
+import unittest
+from SwapBillTest import hook_KeyPair
 from SwapBill import KeyPair
-
-nextI = 0
-def GeneratePrivateKey_ForTests():
-	global nextI
-	result = str(nextI) + '-' * 31
-	nextI += 1
-	return result[:32].encode('ascii')
-def PrivateKeyToPublicKey_ForTests(privateKey):
-	s = privateKey.decode('ascii').strip('-')
-	i = int(s)
-	result = str(i) + '+' * 63
-	return result[:64].encode('ascii')
-def PublicKeyToPubKeyHash_ForTests(publicKey):
-	s = publicKey.decode('ascii').strip('+')
-	i = int(s)
-	result = str(i) + '*' * 19
-	return result[:20].encode('ascii')
-
-KeyPair.GeneratePrivateKey = GeneratePrivateKey_ForTests
-KeyPair.PrivateKeyToPublicKey = PrivateKeyToPublicKey_ForTests
-KeyPair.PublicKeyToPubKeyHash = PublicKeyToPubKeyHash_ForTests
 
 class Test(unittest.TestCase):
 	def test_key_pairs(self):
