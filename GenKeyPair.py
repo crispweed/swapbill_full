42284c8f3ee635e6551b82d5ba8abe4326222e19
Thomas Young <crispweed@upcoder.com>
2014-09-04 10:04:03 +0200

    fixed GenKeyPair.py to work again, but now prints out private key in hex instead of wif format, addresses put into bitcoin or litecoin testnet address format

--- GenKeyPair.py
+++ GenKeyPair.py
@@ -4,12 +4,18 @@ from os import path
 scriptPath = path.dirname(path.abspath(__file__))
 sys.path.append(path.join(scriptPath, 'module'))
 sys.dont_write_bytecode = True
-from SwapBill import KeyPair,Address
+from SwapBill import KeyPair,Address,Util
 
-privateKey = KeyPair.generatePrivateKey()
-print(KeyPair.privateKeyToWIF(privateKey, b'\x80')) # bitcoin mainnet private key address version
-publicKey = KeyPair.privateKeyToPublicKey(privateKey)
-pubKeyHash = KeyPair.publicKeyToPubKeyHash(publicKey)
-address = Address.FromPubKeyHash(b'\x00', pubKeyHash) # bitcoin mainnet address version
-assert Address.ToPubKeyHash(b'\x00', address) == pubKeyHash
-print(address)
+privateKey = KeyPair.GeneratePrivateKey()
+print(Util.toHex(privateKey))
+publicKey = KeyPair.PrivateKeyToPublicKey(privateKey)
+pubKeyHash = KeyPair.PublicKeyToPubKeyHash(publicKey)
+
+#address = Address.FromPubKeyHash(b'\x00', pubKeyHash) # bitcoin mainnet address version
+#assert Address.ToPubKeyHash(b'\x00', address) == pubKeyHash
+#print('bitcoin:', address)
+
+address = Address.FromPubKeyHash(b'\x6f', pubKeyHash)
+assert Address.ToPubKeyHash(b'\x6f', address) == pubKeyHash
+print('bitcoin or litecoin testnet:', address)
+
