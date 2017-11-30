61485724702bb6f88cc621956337dc45b995a2ae
Thomas Young <thomas@upcoder.com>
2017-11-30 08:27:43 +0100

    got GenKeyPair.py working again

--- GenKeyPair.py
+++ GenKeyPair.py
@@ -4,12 +4,12 @@ from os import path
 scriptPath = path.dirname(path.abspath(__file__))
 sys.path.append(path.join(scriptPath, 'module'))
 sys.dont_write_bytecode = True
-from SwapBill import KeyPair,Address
+from SwapBill import Wallet,Address
 
-privateKey = KeyPair.generatePrivateKey()
-print(KeyPair.privateKeyToWIF(privateKey, b'\x80')) # bitcoin mainnet private key address version
-publicKey = KeyPair.privateKeyToPublicKey(privateKey)
-pubKeyHash = KeyPair.publicKeyToPubKeyHash(publicKey)
+keyGen = Wallet.DefaultKeyGenerator()
+privateKey = keyGen.generatePrivateKey()
+print(Address.PrivateKeyToWIF(privateKey, b'\x80')) # bitcoin mainnet private key address version
+pubKeyHash = keyGen.privateKeyToPubKeyHash(privateKey)
 address = Address.FromPubKeyHash(b'\x00', pubKeyHash) # bitcoin mainnet address version
 assert Address.ToPubKeyHash(b'\x00', address) == pubKeyHash
 print(address)
